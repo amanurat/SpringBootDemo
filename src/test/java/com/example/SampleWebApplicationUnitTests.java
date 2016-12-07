d68a71cdad419177e9035f072474bb34ed8b1270
amanurat <assanai17@gmail.com>
2016-12-08 02:45:43 +0700

    Add Unit test with json path

--- src/test/java/com/example/SampleWebApplicationUnitTests.java
+++ src/test/java/com/example/SampleWebApplicationUnitTests.java
@@ -2,49 +2,57 @@ package com.example;
 
 
 import com.example.controller.EmployeeController;
+import com.example.domain.Employee;
+import com.example.service.EmployeeService;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.Arrays;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 
 public class SampleWebApplicationUnitTests {
 
-    @Autowired
-    private WebApplicationContext context;
-
     private MockMvc mvc;
-//
-//    @InjectMocks
-//    lateinit var controller: NewsController
-//    @Mock lateinit var newsService: NewsService
-//    @Mock lateinit var viewNewsRepository: ViewNewsRepository
-//    lateinit var mvc: MockMvc
-//
-//    @Before
-//    fun setup() {
-//        mvc = MockMvcBuilders.standaloneSetup(controller).build()
-//    }
 
     @InjectMocks
     private EmployeeController controller;
 
-
+    @Mock
+    private EmployeeService service;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
+        mvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
 
     @Test
-    public void testHome() throws Exception {
-        this.mvc.perform(get("/employees")).andExpect(status().isOk());
+    public void getAllEmployeeShouldFoundOneRecord() throws Exception {
+
+        Employee employee = new Employee();
+        employee.setId(1);
+        employee.setFirstName("john");
+        employee.setLastName("deo");
+
+        Mockito.when(service.findEmployees()).thenReturn(Arrays.asList(employee));
+
+        this.mvc.perform(get("/employees"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$.[0].id", is(1)));
     }
 
 }
